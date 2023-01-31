a1717ad1f69f7efb0fe2e063bd07b63465fefde3
Prospect138 <highwayman180298@gmail.com>
2023-02-01 02:11:00 +0400

    main returned to work with std::cin

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -18,14 +18,14 @@ int main() {
     std::string line;
     std::vector<std::string> input_query;
     std::vector<std::string> output_query;
-    std::ifstream infile("tsC_case1_input.txt");
+    //std::ifstream infile("tsC_case1_input.txt");
 
     std::string count_fillstr;
-    getline(infile, count_fillstr);
+    getline(std::cin, count_fillstr);
     int count_fill = stoi(count_fillstr);
 
     for (int i = 0; i < count_fill; ++i){
-        getline(infile, line);
+        getline(std::cin, line);
         input_query.push_back(std::move(line));
     }
     std::sort(input_query.begin(), input_query.end(), std::greater<std::string>());
@@ -34,11 +34,11 @@ int main() {
     }
     
     std::string count_requeststr;
-    getline(infile, count_requeststr);
+    getline(std::cin, count_requeststr);
     int count_request = stoi(count_requeststr);
 
     for (int i = 0; i < count_request; ++i){
-        getline(infile, line);
+        getline(std::cin, line);
         output::ReadOutput(line, catalog);
     }
     return 0;
